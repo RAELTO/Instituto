d02127dc678b3fe8cdd7e0afc2288ad76f2f5206
Rael <elias-toro@outlook.com>
2022-07-28 14:10:45 -0500

    course status updated from DB and implemented in JSON results by endpoint

--- controllers/cursoController.js
+++ controllers/cursoController.js
@@ -4,7 +4,7 @@ const Course = require('../models/cursos');
 const getAllCourses = async(req = request, res = response) => {//obtener todos los cursos
     await Course.findAll({attributes:[
         'id', 'nombre_curso', 'descripcion', 'cupo_disponible',
-        'area_estudio_id', 'fecha_limite_curso'
+        'area_estudio_id', 'fecha_limite_curso', 'estado_curso', 'img_curso'
     ]})
         .then(course => {
             const data = JSON.stringify(course);
@@ -21,7 +21,7 @@ const getOneCourse = async(req = request, res = response) => {
     //res.send(`Get course ${req.params.id}`);
     await Course.findOne({attributes:[
         'id', 'nombre_curso', 'descripcion', 'cupo_disponible',
-        'area_estudio_id', 'fecha_limite_curso'
+        'area_estudio_id', 'fecha_limite_curso', 'estado_curso', 'img_curso'
     ], where: { id: req.params.id } })
         .then(course => {
             const data = JSON.stringify(course);
