a66ec56d3d123457ec25e1e4f4a3b1b08fcb6eca
tetsudevelopment <bresmacawork@gmail.com>
2022-07-28 15:58:20 -0500

    Core frontend

---
+++ instituto/src/components/PruebaSemi.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hello">
+    <h1>Hola Mundo</h1> 
+    <button class="btn btn-primary">hola</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PruebaSemi',
+}
+</script>
