1556a68b3a1249de96cb6eb4f01df247bb476df8
tetsudevelopment <bresmacawork@gmail.com>
2022-08-04 19:56:47 -0500

    Admin

--- instituto/src/components/PersonalAdminInst.vue
+++ instituto/src/components/PersonalAdminInst.vue
@@ -1,126 +1,439 @@
 <template>
-<!-- Buttons -->
+  <!-- Buttons -->
   <section>
     <div class="container mt-5 p-3">
-      <button class="btn m-2 fw-bold text-white " :disabled="view===0" @click="user"> Usuarios</button>
-      <button class="btn m-2 fw-bold text-white" :disabled="view===1" @click="category">Categoría</button>
-      <button class="btn m-2 fw-bold text-white" :disabled="view===2" @click="courses">Cursos</button>
+      <button
+        class="btn m-2 fw-bold text-white"
+        :disabled="view === 0"
+        @click="user"
+      >
+        Usuarios
+      </button>
+      <button
+        class="btn m-2 fw-bold text-white"
+        :disabled="view === 1"
+        @click="category"
+      >
+        Categoría
+      </button>
+      <button
+        class="btn m-2 fw-bold text-white"
+        :disabled="view === 2"
+        @click="courses"
+      >
+        Cursos
+      </button>
     </div>
   </section>
   <!-- Tables -->
   <section>
-     <!--User  -->
-    <section v-if="view===0">
+    <!--User  -->
+    <section v-if="view === 0">
       <div class="container">
-      <button class="btn p-1 m-2 fw-bold text-white">Crear usuario</button>
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Apellidos</th>
-            <th scope="col">Correo</th>
-            <th scope="col">Estado</th>
-            <th scope="col">Opciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td><span>Brayan Esteban</span></td>
-            <td><span>Marin Calderon</span></td>
-            <td><span>demo@demo.com</span></td>
-            <td><span>Activo</span></td>
-            <td><button class="btn"></button></td>
-          </tr>
-        </tbody>
-      </table>
+        <button
+          class="btn p-1 m-2 fw-bold text-white"
+          data-bs-toggle="modal"
+          data-bs-target="#userCreate"
+        >
+          <i class="bi bi-person-plus-fill p-3"></i>
+        </button>
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Apellidos</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Estado</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><span>Brayan Esteban</span></td>
+              <td><span>Marin Calderon</span></td>
+              <td><span>demo@demo.com</span></td>
+              <td><span>Activo</span></td>
+              <td>
+                <button
+                  class="btn text-white"
+                  data-bs-toggle="modal"
+                  data-bs-target="#userEdit"
+                >
+                  <i class="bi bi-eye-fill"></i>
+                </button>
+                <button class="btn text-white danger ms-1">
+                  <i class="bi bi-trash3-fill"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- Modal  Crear usuario/Matricula-->
+      <div
+        class="modal fade"
+        id="userCreate"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title fw-bold" id="staticBackdropLabel">
+                Agregar Usuario
+              </h5>
+              <p
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></p>
+            </div>
+            <div class="modal-body">
+              <div class="container">
+                <div class="row">
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Nombre"
+                      aria-label="Nombre"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Apellido"
+                      aria-label="Apellido"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Fecha de Nacimiento"
+                      aria-label="Nacimiento"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Teléfono"
+                      aria-label="Telefono"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Dirección"
+                      aria-label="Direccion"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Correo"
+                      aria-label="Correo"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Tipo de documento</option>
+                      <option value="1">TI</option>
+                    </select>
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="No. Documento"
+                      aria-label="NoDocumento"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-12">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Tipo de rol</option>
+                      <option value="1">admin</option>
+                    </select>
+                  </div>
+                  <div class="input-group mb-3">
+                    <label class="fw-bold col-12 mb-2"> Cargar documento</label>
+                    <input
+                      type="file"
+                      class="form-control"
+                      id="inputGroupFile02"
+                    />
+                    <label class="input-group-text" for="inputGroupFile02"
+                      >Upload</label
+                    >
+                  </div>
+                  <div class="input-group mb-3">
+                    <label class="fw-bold col-12 mb-2"> Cargar Imagen</label>
+                    <input
+                      type="file"
+                      class="form-control"
+                      id="inputGroupFile02"
+                    />
+                    <label class="input-group-text" for="inputGroupFile02"
+                      >Upload</label
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary fw-bold"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary fw-bold">
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Editar Usuario -->
+      <div
+        class="modal fade"
+        id="userEdit"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title fw-bold" id="staticBackdropLabel">
+                Editar Usuario
+              </h5>
+              <p
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></p>
+            </div>
+            <div class="modal-body">
+              <div class="container">
+                <div class="row">
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Nombre"
+                      aria-label="Nombre"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Apellido"
+                      aria-label="Apellido"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Fecha de Nacimiento"
+                      aria-label="Nacimiento"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Teléfono"
+                      aria-label="Telefono"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Dirección"
+                      aria-label="Direccion"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="Correo"
+                      aria-label="Correo"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-6">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Tipo de documento</option>
+                      <option value="1">TI</option>
+                    </select>
+                  </div>
+                  <div class="mb-3 col-6">
+                    <input
+                      type="number"
+                      class="form-control"
+                      placeholder="No. Documento"
+                      aria-label="NoDocumento"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                  <div class="mb-3 col-12">
+                    <select
+                      class="form-select"
+                      disabled
+                      aria-label="Default select example"
+                    >
+                      <option selected>Tipo de rol</option>
+                      <option value="1">admin</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary fw-bold"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary fw-bold">
+                Guardar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <!--Category  -->
-    <section v-if="view===1">
+    <section v-if="view === 1">
       <div class="container">
-      <button class="btn p-1 m-2 fw-bold text-white">Crear usuario</button>
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-        </tbody>
-      </table>
-      </div> 
-    </section>
-    <!-- Courses -->
-    <section v-if="view===2">
-      <div class="container">
-      <button class="btn p-1 m-2 fw-bold text-white">Crear usuario</button>
-      <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-        </tbody>
-      </table>
+        <button class="btn p-1 m-2 fw-bold text-white">Crear usuario</button>
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </section>
-
+    <!-- Courses -->
+    <section v-if="view === 2">
+      <div class="container">
+        <button class="btn p-1 m-2 fw-bold text-white">Crear usuario</button>
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
   </section>
 </template>
 <script>
 export default {
   data() {
-    return{
-      view:0,
-    }
-  },   
-  methods:{
-    user(){
-      this.view=0;
+    return {
+      view: 0,
+    };
+  },
+  methods: {
+    user() {
+      this.view = 0;
     },
-    category(){
-      this.view=1;
+    category() {
+      this.view = 1;
     },
-    courses(){
-      this.view=2;
-    }
-  }
-}
+    courses() {
+      this.view = 2;
+    },
+  },
+};
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Poppins:wght@300;600&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Poppins:wght@300;600&display=swap");
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css");
-  *{
-    font-family: 'Oswald', sans-serif;
-    font-family: 'Poppins', sans-serif;
-  }
-  :root{
-    --first: #2a8c4a;
-    --second: #64c27b;
-    --third:#9bfab0;
-    --fourth:#d0fdd7;
-    --fifth:#ffffff;
-  }
-button{
+* {
+  font-family: "Oswald", sans-serif;
+  font-family: "Poppins", sans-serif;
+}
+:root {
+  --first: #2a8c4a;
+  --second: #64c27b;
+  --third: #9bfab0;
+  --fourth: #d0fdd7;
+  --fifth: #ffffff;
+}
+button {
   background: var(--first) !important;
+}
+.danger {
+  background: rgb(237, 52, 52) !important;
+}
+.modal-content {
+  background: var(--fourth);
 }
 </style>
