b0b15070395e5aa34da3bee53e9f972cbe6817ef
tetsudevelopment <bresmacawork@gmail.com>
2022-08-14 20:56:14 -0500

    vee-validate

--- instituto/src/components/HelloWorld.vue
+++ instituto/src/components/HelloWorld.vue
@@ -1,40 +1,62 @@
 <template>
-  <v-carousel v-model="model">
-    <v-carousel-item
-      v-for="(color, i) in colors"
-      :key="color"
-    >
-      <v-sheet
-        :color="color"
-        height="100%"
-        tile
-      >
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="text-h2">
-            Slide {{ i + 1 }}
-          </div>
-        </v-row>
-      </v-sheet>
-    </v-carousel-item>
-  </v-carousel>
+  <div id="app">
+    <Form @submit="onSubmit">
+      <div class="mb-3 col-6">
+        <Field
+          type="text"
+          name="nombre"
+          class="form-control"
+          placeholder="Nombre"
+          aria-label="Nombre"
+          aria-describedby="basic-addon1"
+          :rules="validateNombre"
+          v-model="Usuario.nombre"
+        />
+        <ErrorMessage name="nombre" id="error"/>
+      </div>
+      <button>Hola</button>
+    </Form>
+  </div>
 </template>
 <script>
-
+import { Form, Field, ErrorMessage } from 'vee-validate';
 export default {
-  name: 'HelloWorld',
-   data: () => ({
-      model: 0,
-      colors: [
-        'primary',
-        'secondary',
-        'yellow darken-2',
-        'red',
-        'orange',
-      ],
-    }),
-}
+  data(){
+    return{
+      Usuario:{
+        nombre:''
+      }
+    }
+  },
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  methods: {
+        onSubmit(values) {
+      console.log(values, null, 2);
+    },
+    validateEmail(value) {
+      // if the field is empty
+      if (!value) {
+        return 'This field is required';
+      }
+      // if the field is not a valid email
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (!regex.test(value)) {
+        return 'This field must be a valid email';
+      }
+      // All is good
+      return true;
+    },
+    validateNombre(value){
+      if(!value){
+        return 'El nombre es requerido';
+      }
+      // All is good
+      return true;
+    },
+  },
+};
 </script>
